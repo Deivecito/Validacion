7508fef5d70a6be313049dacff318699ec32d0ba
Deivecito <agustingonzalez200133@gmail.com>
2023-08-11 16:44:41 -0300

    validacion

--- js/validacion.js
+++ js/validacion.js
@@ -1,7 +1,64 @@
-function showAlertSuccess() {
-    document.getElementById("alert-success").classList.add("show");
-}
 
-function showAlertError() {
-    document.getElementById("alert-danger").classList.add("show");
-}
+document.addEventListener("DOMContentLoaded", function () {
+    let regBtn = document.getElementById("regBtn");
+    let nombre = document.getElementById("nombre");
+    let apellido = document.getElementById("apellido");
+    let email = document.getElementById("email");
+    let password1 = document.getElementById("password1");
+    let password2 = document.getElementById("password2");
+    let terminos = document.getElementById("terminos");
+    let alertSuccess = document.getElementById("alert-success");
+    let alertDanger = document.getElementById("alert-danger");
+
+    regBtn.addEventListener("click", function () {
+        alertSuccess.classList.remove("show");
+
+        if (
+            nombre.value === "" ||
+            apellido.value === "" ||
+            email.value === "" ||
+            password1.value === "" ||
+            password2.value === "" ||
+            !terminos.checked
+        ) {
+            alertDanger.classList.add("show")
+            setTimeout(() => {
+                alertDanger.classList.remove("show")
+            }, 3000);
+            return;
+        }
+
+        if (password1.value.length < 6) {
+            alertDanger.classList.add("show")
+            setTimeout(() => {
+                alertDanger.classList.remove("show")
+            }, 3000);
+            return;
+            
+        }
+
+        if (password1.value !== password2.value) {
+            alertDanger.classList.add("show")
+            setTimeout(() => {
+                alertDanger.classList.remove("show")
+            }, 3000);
+            return;
+            
+        }
+else
+        alertSuccess.classList.add("show");
+        setTimeout(() => {
+            alertSuccess.classList.remove("show")
+        }, 3000);
+        return;
+    })
+})
+
+
+
+
+
+
+     
+    
+
